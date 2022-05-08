b50386e73924047647a6b5a43691aa7087871660
Heather Carstensen <heather.carstensen.741@my.csun.edu>
2022-05-08 12:31:33 -0700

    Updates

---
+++ Script/odr3_slider_app/odr3_slider/app.R
@@ -0,0 +1,59 @@
+#### Making a shiny app with an image slider ####
+#### Following code for shiny.slider on Github ####
+#### See: https://github.com/Toniiiio/shiny.slider ####
+#### This app is hosted at:
+#### https://h-carstensen.shinyapps.io/odr3_slider/
+
+
+#### Loading library ####
+
+library(shiny)
+
+### Create the user interface, using the default settings and files
+# provided in the shiny.slider repository on Github
+
+ui <- fluidPage(
+  
+  tags$script(src = "jquery-1.6.1.min.js"),
+  tags$script(src = "jquery-ui-1.8.13.custom.min.js"),
+  tags$script(src = "jquery.beforeafter-1.4.min.js"),
+  
+  tags$script("
+            $(function() {
+                $('#slider').beforeAfter({
+                    introDelay: 2000,
+                    imagePath: 'img/',
+                    introDuration: 500,
+                    showFullLinks: false
+                })
+              });
+  "),
+  
+  mainPanel(
+    
+    tags$div(
+      id = "slider",
+      img(
+        src = "img/odr3_wt.jpeg",   #Replace the default image with my image
+        width = 800, 
+        height = 533
+      ),
+      
+      img(
+        src = "img/odr3_mut.jpeg",   #Replace the default image with my image
+        width = 800, 
+        height = 533
+      )
+    )
+  )
+  
+)
+
+
+#### Set up the server
+
+server <- function(input, output, session){}
+
+#### Run the app
+
+shinyApp(ui, server)
